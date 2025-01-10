9ae72b0187022bdde0b1a471a4361c82c9fa5930
treefroglado <treefroglado>
2025-01-10 12:33:07 +0000

    Commint new change

--- Analytics Script RoL Band.py
+++ Analytics Script RoL Band.py
@@ -1,3 +1,21 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC SELECT SUM(Cedant_Limit) FROM platinum.overall_region
+# MAGIC WITH TotalPremium AS (
+# MAGIC   SELECT SUM(Total_Premium) as Total_Premium
+# MAGIC   FROM tangency_dlextech_demo.gold.rol_band_input
+# MAGIC   WHERE Intl___US = "US" AND Region_Type = "US" AND Program = "Property - Aggregate"
+# MAGIC )
+# MAGIC SELECT 
+# MAGIC   Mid_RoL, 
+# MAGIC   SUM(SUM(Total_Premium)) OVER (ORDER BY Mid_RoL DESC) / (SELECT Total_Premium FROM TotalPremium) * 100 as CumulativePremiumPercentage 
+# MAGIC FROM tangency_dlextech_demo.gold.rol_band_input 
+# MAGIC WHERE Intl___US = "US" AND Region_Type = "US" AND Program = "Property - Aggregate"
+# MAGIC GROUP BY Mid_RoL
+# MAGIC SORT BY Mid_RoL DESC
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Text
